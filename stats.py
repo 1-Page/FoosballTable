80c78453bd3624d20ea3f25c4f7f23dcacec2bba
pizzato <luiz@1-page.com>
2015-11-05 18:38:18 +1100

    Basic method for stats #6

--- stats.py
+++ stats.py
@@ -21,7 +21,7 @@ class Stats:
                  attacker_id=None,
                  defender_id=None,
                  team_id=None,
-                 wins=0, draws=0, loses=0,
+                 wins=0, draws=0, losses=0,
                  goals_pro=0, goals_against=0,
                  elo_rating=config.INITIAL_RATING,
                  timestamp=tools.get_timestamp_for_now()):
@@ -36,7 +36,7 @@ class Stats:
 
         self.wins = wins
         self.draws = draws
-        self.loses = loses
+        self.losses = losses
 
         self.goals_pro = goals_pro
         self.goals_against = goals_against
@@ -45,6 +45,15 @@ class Stats:
 
         self.timestamp = timestamp
 
+
+    def update(self, i_wins=0, i_draws=0, i_losses=0, i_goals_pro=0, i_goals_against=0, i_elo_rating=0, timestamp=tools.get_timestamp_for_now()):
+        self.wins += i_wins
+        self.draws += i_draws
+        self.losses += i_losses
+        self.goals_pro += i_goals_pro
+        self.goals_against += i_goals_against
+        self.elo_rating += i_elo_rating
+        self.timestamp = timestamp
 
 class StatsDB:
     con = None
@@ -58,7 +67,7 @@ class StatsDB:
         s = """
               CREATE TABLE IF NOT EXISTS stats (stats_id INTEGER PRIMARY KEY AUTOINCREMENT,
                                                 player_id INT, attacker_id INT, defender_id INT, team_id INT,
-                                                wins INT, draws INT, loses INT,
+                                                wins INT, draws INT, losses INT,
                                                 goals_pro INT, goals_against INT,
                                                 elo_rating NUMBER,
                                                 timestamp TEXT)
@@ -66,34 +75,177 @@ class StatsDB:
         cur.execute(s)
         self.con.commit()
 
+    def get_stat(self, player_id=None, attacker_id=None, defender_id=None, team_id=None):
+        cur = self.con.cursor()
 
-    def add_stats(self,
-                  player_id=None,
-                  attacker_id=None,
-                  defender_id=None,
-                  team_id=None,
-                  wins=0, draws=0, loses=0,
-                  goals_pro=0, goals_against=0,
-                  elo_rating=config.INITIAL_RATING,
-                  timestamp=tools.get_timestamp_for_now()):
+        ids_dict = dict(player_id=player_id, attacker_id=attacker_id, defender_id=defender_id, team_id=team_id)
 
-            stat_dict = dict(player_id=player_id, attacker_id=attacker_id, defender_id=defender_id, team_id=team_id,
-                             wins=wins, draws=draws, loses=loses,
-                             goals_pro=goals_pro, goals_against=goals_against,
-                             elo_rating=elo_rating, timestamp=timestamp)
+        q = """
+                SELECT player_id, attacker_id, defender_id, team_id, wins, draws, losses, goals_pro, goals_against, elo_rating, timestamp
+                FROM STATS
+                WHERE player_id=:player_id AND attacker_id=:attacker_id AND defender_id=:defender_id AND team_id=:team_id
+                ORDER BY stats_id DESC
+            """
+        cur.execute(q, ids_dict)
+        stat_exits = cur.fetchone()
+        if not stat_exits:
+            stats = Stats(ids_dict)
+        else:
+            player_id, attacker_id, defender_id, team_id, wins, draws, losses, goals_pro, goals_against, elo_rating, timestamp = stat_exits
 
-            s = """
-                  INSERT INTO stats(player_id, attacker_id, defender_id, team_id,
-                                    wins, draws, loses,
-                                    goals_pro, goals_against,
-                                    elo_rating, timestamp)
-                          VALUES(:player_id, :attacker_id, :defender_id, :team_id,
-                                 :wins, :draws, :loses,
-                                 :goals_pro, :goals_against,
-                                 :elo_rating, :timestamp)
-                """
+            stats = Stats(player_id=player_id, attacker_id=attacker_id, defender_id=defender_id, team_id=team_id,
+                          wins=wins, draws=draws, losses=losses,
+                          goals_pro=goals_pro, goals_against=goals_against, elo_rating=elo_rating, timestamp=timestamp)
 
-            cur = self.con.cursor()
-            cur.execute(s, stat_dict)
-            self.con.commit()
+        return stats
+
+
+    def increment_stats(self,
+                        player_id=None,
+                        attacker_id=None,
+                        defender_id=None,
+                        team_id=None,
+                        i_wins=0, i_draws=0, i_losses=0,
+                        i_goals_pro=0, i_goals_against=0,
+                        i_elo_rating=0,
+                        timestamp=tools.get_timestamp_for_now()):
+        """
+            Increment the stats of a player, attacker, defender, or team based by certain values
+        """
+
+        cur = self.con.cursor()
+        stats = self.get_stat(player_id=player_id, attacker_id=attacker_id, defender_id=defender_id, team_id=team_id)
+
+        stats.update(i_wins=i_wins, i_draws=i_draws, i_losses=i_losses, i_goals_pro=i_goals_pro, i_goals_against=i_goals_against, i_elo_rating=i_elo_rating, timestamp=timestamp)
+
+
+        s = """
+              INSERT INTO stats(player_id, attacker_id, defender_id, team_id,
+                                wins, draws, losses,
+                                goals_pro, goals_against,
+                                elo_rating, timestamp)
+                      VALUES(:player_id, :attacker_id, :defender_id, :team_id,
+                             :wins, :draws, :losses,
+                             :goals_pro, :goals_against,
+                             :elo_rating, :timestamp)
+            """
+
+
+        cur.execute(s, stats.__dict__)
+        self.con.commit()
+
+        stats.stats_id = cur.lastrowid
+
+        return stats
+
+
+
+
+    def add_game(self, game):
+        if not game.ended:
+            raise Exception("Game not ended")
+
+        """
+        game.timestamp = timestamp
+        game.team_left = team_left
+        game.team_right = team_right
+        game.score_left = score_left
+        game.score_right = score_right
+        """
+
+        stats_timestamp = tools.get_timestamp_for_now()
+
+        # Get all stats
+
+        elo_team_left = self.get_stat(team_id=game.team_left.team_id).elo_rating
+        elo_team_right = self.get_stat(team_id=game.team_right.team_id).elo_rating
+        elo_attack_left = self.get_stat(attacker_id=game.team_left.attack_player.player_id).elo_rating
+        elo_defense_left = self.get_stat(defender_id=game.team_left.defense_player.player_id).elo_rating
+        elo_attack_right = self.get_stat(attacker_id=game.team_right.attack_player.player_id).elo_rating
+        elo_defense_right = self.get_stat(defender_id=game.team_right.defense_player.player_id).elo_rating
+
+        elo_player_attack_left = self.get_stat(player_id=game.team_left.attack_player.player_id).elo_rating
+        elo_player_defense_left = self.get_stat(player_id=game.team_left.defense_player.player_id).elo_rating
+        elo_player_attack_right = self.get_stat(player_id=game.team_right.attack_player.player_id).elo_rating
+        elo_player_defense_right = self.get_stat(player_id=game.team_right.defense_player.player_id).elo_rating
+
+        # Score percentages
+
+        if (game.score_left+game.score_right) != 0:
+            score_p_left = game.score_left / float(game.score_left+game.score_right)
+            score_p_right = 1.0 - score_p_left
+        else:
+            score_p_left = 0.5
+            score_p_right = 0.5
+
+
+        i_elo_team_left = rating_increment(score_perc=score_p_left, diff_ratings=(elo_team_left - elo_attack_right))
+        i_elo_team_right = rating_increment(score_perc=score_p_right, diff_ratings=(elo_attack_right - elo_team_left))
+
+        i_elo_left_players_pos = rating_increment(score_perc=score_p_left, diff_ratings=((elo_attack_left+elo_defense_left) - (elo_attack_right+elo_defense_right)))
+        i_elo_right_players_pos = rating_increment(score_perc=score_p_right, diff_ratings=((elo_attack_right+elo_defense_right)-(elo_attack_left+elo_defense_left)))
+
+        i_elo_left_players_ind = rating_increment(score_perc=score_p_left, diff_ratings=((elo_player_attack_left+elo_player_defense_left) - (elo_player_attack_right+elo_player_defense_right)))
+        i_elo_right_players_ind = rating_increment(score_perc=score_p_right, diff_ratings=((elo_player_attack_right+elo_player_defense_right) - (elo_player_attack_left+elo_player_defense_left)))
+
+        # Fixed timestamp per game/stats
+        timestamp=tools.get_timestamp_for_now()
+
+        # Increments for left or right teams
+        left_dict_goals_increment = dict(i_wins=int(game.score_left>game.score_right),
+                                         i_draws=int(game.score_left==game.score_right),
+                                         i_losses=int(game.score_left<game.score_right),
+                                         i_goals_pro=game.score_left,
+                                         i_goals_against=game.score_right,
+                                         timestamp=timestamp)
+
+        right_dict_goals_increment = dict(i_wins=int(game.score_right>game.score_left),
+                                          i_draws=int(game.score_left==game.score_right),
+                                          i_losses=int(game.score_right<game.score_left),
+                                          i_goals_pro=game.score_right,
+                                          i_goals_against=game.score_left,
+                                          timestamp=timestamp)
+
+
+
+        self.increment_stats(team_id=game.team_left.team_id,
+                             i_elo_rating=i_elo_team_left,
+                             **left_dict_goals_increment)
+
+        self.increment_stats(team_id=game.team_right.team_id,
+                             i_elo_rating=i_elo_team_right,
+                             **right_dict_goals_increment)
+
+        self.increment_stats(attacker_id=game.team_left.attack_player.player_id,
+                             i_elo_rating=i_elo_left_players_pos,
+                             **left_dict_goals_increment)
+
+        self.increment_stats(defender_id=game.team_left.defense_player.player_id,
+                             i_elo_rating=i_elo_left_players_pos,
+                             **left_dict_goals_increment)
+
+        self.increment_stats(attacker_id=game.team_right.attack_player.player_id,
+                             i_elo_rating=i_elo_right_players_pos,
+                             **right_dict_goals_increment)
+
+        self.increment_stats(defender_id=game.team_right.defense_player.player_id,
+                             i_elo_rating=i_elo_right_players_pos,
+                             **right_dict_goals_increment)
+
+        self.increment_stats(player_id=game.team_left.attack_player.player_id,
+                             i_elo_rating=i_elo_left_players_ind,
+                             **left_dict_goals_increment)
+
+        self.increment_stats(player_id=game.team_left.defense_player.player_id,
+                             i_elo_rating=i_elo_left_players_ind,
+                             **left_dict_goals_increment)
+
+        self.increment_stats(player_id=game.team_right.attack_player.player_id,
+                             i_elo_rating=i_elo_right_players_ind,
+                             **right_dict_goals_increment)
+
+        self.increment_stats(player_id=game.team_right.defense_player.player_id,
+                             i_elo_rating=i_elo_right_players_ind,
+                             **right_dict_goals_increment)
+
 
